b63ce34646c3a28d36f18131faa1ff8c5b9b6627
Muhammad Khalish Zhafran <92090918+khalishzhafran@users.noreply.github.com>
2022-03-12 22:31:34 +0800

    Update nodeList.js 3.0

--- nodeList.js
+++ nodeList.js
@@ -1,69 +1,46 @@
-class Node {
+class LinkedList {
     constructor(data) {
         this.data = data;
         this.next = null;
         this.prev = null;
-    }
-}
-
-class LinkedList {
-    constructor() {
         this.head = null;
         this.tail = null;
         this.length = 0;
     }
 
     add(data) {
-        let node = new Node(data);
+        let node = new LinkedList(data);
         if (this.head === null) {
             this.head = node;
             this.tail = node;
-        } else {
-            this.tail.next = node;
-            node.prev = this.tail;
-            this.tail = node;
-        }
+        } else [this.tail.next, node.prev, this.tail] = [node, this.tail, node];
+
         this.length++;
-        return this;
+        return node;
     }
 
     insertAtIndex(data, index) {
-        if (index > this.length) {
-            return false;
-        }
-        let node = new Node(data);
+        if (index > this.length || index < 0) return null;
+
+        let node = new LinkedList(data);
         let current = this.head;
         let count = 0;
-        if (index === 0) {
-            node.next = current;
-            current.prev = node;
-            this.head = node;
-        } else if (index === this.length) {
+        if (index === 0) [node.next, current.prev, this.head] = [current, node, node];
+        else {
             while (count < index - 1) {
                 current = current.next;
                 count++;
             }
-            current.next = node;
-            node.prev = current;
-            this.tail = node;
-        } else {
-            while (count < index - 1) {
-                current = current.next;
-                count++;
-            }
-            node.next = current.next;
-            current.next.prev = node;
-            node.prev = current;
-            current.next = node;
+
+            if (index === this.length) [current.next, node.prev, this.tail] = [node, current, node];
+            else [node.next, current.next.prev, node.prev, current.next] = [current.next, node, current, node];
         }
         this.length++;
-        return this;
+        return current.data;
     }
 
     removeAt(index) {
-        if (index > this.length - 1 || index < 0) {
-            return null;
-        }
+        if (index > this.length - 1 || index < 0) return null;
 
         let current = this.head;
         let count = 0;
@@ -71,16 +48,13 @@ class LinkedList {
             if (count === index) {
                 let prev = current.prev;
                 let next = current.next;
-                if (prev) {
-                    prev.next = next;
-                } else {
-                    this.head = next;
-                }
-                if (next) {
-                    next.prev = prev;
-                } else {
-                    this.tail = prev;
-                }
+
+                if (prev) prev.next = next;
+                else this.head = next;
+
+                if (next) next.prev = prev;
+                else this.tail = prev;
+
                 break;
             }
             current = current.next;
